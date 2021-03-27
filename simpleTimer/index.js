c21f89b530d0a1c266dcf56df9c8364ce8f350da
Anthonalex <Anthonalex@users.noreply.github.com>
2021-03-27 12:06:10 +0400

    start, pause & clear btns done,works fanstastic.

--- simpleTimer/index.js
+++ simpleTimer/index.js
@@ -1,23 +1,62 @@
 const minDisplay = document.querySelector(".min-cont");
 const secDisplay = document.querySelector(".sec-cont");
 const startBtn = document.querySelector(".start");
-const duration = minDisplay.value * 60 + secDisplay.value;
+const resetBtn = document.querySelector(".reset");
+const finishText = document.querySelector(".finish-text");
+const modal = document.querySelector(".modal");
+
+var timer;
 
 const startTimer = (duration, minDisplay, secDisplay) => {
-  let timer = duration,
-    min,
-    sec;
-  setInterval(() => {
-    min = parseInt(timer / 60);
-    sec = parseInt(timer % 60);
+  --duration;
+  var min, sec;
+
+  minDisplay.setAttribute("disabled", true);
+  secDisplay.setAttribute("disabled", true);
+
+  startBtn.textContent = "PAUSE";
+
+  timer = setInterval(() => {
+    min = parseInt(duration / 60);
+    sec = parseInt(duration % 60);
 
     minDisplay.value = min;
     secDisplay.value = sec;
 
-    --timer;
+    if (duration > 0) {
+      --duration;
+    } else {
+      finishText.style.display = "block";
+      modal.style.display = "block";
+    }
   }, 1000);
 };
 
+const pauseTimer = () => {
+  clearInterval(timer);
+  startBtn.textContent = "START";
+};
+
+let count = 0;
+
 startBtn.addEventListener("click", () => {
-  startTimer(duration, minDisplay, secDisplay);
+  if (count % 2 === 0) {
+    const duration = Number(minDisplay.value) * 60 + Number(secDisplay.value);
+
+    startTimer(duration, minDisplay, secDisplay);
+  } else {
+    pauseTimer();
+  }
+
+  count++;
+});
+
+finishText.addEventListener("click", () => {
+  window.location.reload();
+});
+
+resetBtn.addEventListener("click", () => {
+  pauseTimer();
+  minDisplay.value = "1";
+  secDisplay.value = "0";
 });
