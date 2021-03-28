ed4cc16f015a07bc9cbb0728cb494ecb27bd6160
Anthonalex <Anthonalex@users.noreply.github.com>
2021-03-28 19:37:54 +0400

    dog breeds image finder done,works well

--- dogBreeds/index.js
+++ dogBreeds/index.js
@@ -1,7 +1,48 @@
 const selector = document.querySelector("#selector");
 const breedName = document.querySelector(".breed-name");
 const searchBtn = document.querySelector(".search-btn");
+const dogPic = document.querySelector(".dog-pic");
+const pointerDrawing = document.querySelector(".pointer");
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// fetch("https://dog.ceo/api/breeds/list/all")
+//   .then((x) => {
+//     return x.json();
+//   })
+//   .then((x) => {
+//     console.log(x);
+//   });
+
+const getDogPicture = (breed, subBreed) => {
+  let url = `https://dog.ceo/api/breed/${breed}`;
+
+  if (subBreed !== undefined) {
+    url += `/${subBreed}`;
+  }
+
+  if (breed !== "undefined") {
+    //undefined in string because it is selector's value,
+    breedName.textContent = selector.value;
+
+    fetch(`${url}/images`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((imgs) => {
+        let random = getRandomInt(imgs.message.length - 1);
+        dogPic.src = imgs.message[random];
+      });
+  } else {
+    throw new Error("please select a breed name");
+  }
+};
 
 searchBtn.addEventListener("click", () => {
-  breedName.textContent = selector.value;
+  let inputValue = selector.value;
+  let [breed, subBreed] = inputValue.split(" ");
+
+  getDogPicture(breed, subBreed);
 });
